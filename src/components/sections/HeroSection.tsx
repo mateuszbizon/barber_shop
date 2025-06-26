08ec171a5e44c18f63535704055f21822f5f033b
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-26 11:42:58 +0200

    added animation to hero

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,19 +1,36 @@
+"use client"
+
 import React from 'react'
 import Container from '../ui/container'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 import Shadow from '../ui/shadow'
 import { HERO_IMAGE } from '@/constants/images'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 function HeroSection() {
+    useGSAP(() => {
+        gsap.fromTo(".text-animate", {
+            opacity: 0,
+            y: 50
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power2.out",
+            stagger: 0.3,
+        })
+    }, [])
+
   return (
-    <header style={{ backgroundImage: `url(${HERO_IMAGE.src})` }} className='header-section-height z-0 relative bg-cover bg-center bg-no-repeat'>
+    <header id='header' style={{ backgroundImage: `url(${HERO_IMAGE.src})` }} className='header-section-height z-0 relative bg-cover bg-center bg-no-repeat'>
         <Shadow />
         <Container>
             <div className='flex flex-col justify-center gap-5 md:gap-10 h-full'>
-                <h1 className='heading1 text-center text-light'>Najlepszy Barber Shop w Polsce</h1>
-                <p className='bigger-text text-center text-light'>Zapraszamy do naszego lokalu w Warszawie.</p>
-                <div className='flex justify-center'>
+                <h1 className='text-animate heading1 text-center text-light'>Najlepszy Barber Shop w Polsce</h1>
+                <p className='text-animate bigger-text text-center text-light'>Zapraszamy do naszego lokalu w Warszawie.</p>
+                <div className='text-animate flex justify-center'>
                     <Button size={"lg"} className='text-lg' asChild>
                         <Link href={"https://booksy.com/pl-pl/"} target='_blank'>
                             Zarezerwuj
